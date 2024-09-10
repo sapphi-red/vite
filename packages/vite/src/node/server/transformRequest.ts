bac71c9ba70a47b2617fd1384e97f2f39f79dcea
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-09-10 14:21:59 +0900

    wip

--- packages/vite/src/node/server/transformRequest.ts
+++ packages/vite/src/node/server/transformRequest.ts
@@ -422,8 +422,6 @@ async function loadAndTransform(
         etag: getEtag(code, { weak: true }),
       } satisfies TransformResult)
 
-  // eslint-disable-next-line no-console
-  console.log('loadAndTransform', timestamp, mod.lastInvalidationTimestamp)
   // Only cache the result if the module wasn't invalidated while it was
   // being processed, so it is re-processed next time if it is stale
   if (timestamp > mod.lastInvalidationTimestamp)
@@ -518,16 +516,14 @@ async function handleModuleSoftInvalidation(
     }
   }
 
-  // eslint-disable-next-line no-console
-  console.log(
-    'handleModuleSoftInvalidation',
-    timestamp,
-    mod.lastInvalidationTimestamp,
-  )
   // Only cache the result if the module wasn't invalidated while it was
   // being processed, so it is re-processed next time if it is stale
   if (timestamp > mod.lastInvalidationTimestamp)
     environment.moduleGraph.updateModuleTransformResult(mod, result)
+  else {
+    // eslint-disable-next-line no-console
+    console.log('handleModuleSoftInvalidation', 'skipped update', mod.url)
+  }
 
   return result
 }
